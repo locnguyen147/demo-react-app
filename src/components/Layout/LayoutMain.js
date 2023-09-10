697eb6aab861df29a3c06e107718add8ebb8f91b
NguyenLoc <nguyentanloc140799@gmail.com>
2023-09-10 13:18:52 +0700

    add listmenu in sidebar

--- src/components/Layout/LayoutMain.js
+++ src/components/Layout/LayoutMain.js
@@ -1,6 +1,12 @@
 import React from 'react'
-import {Box} from '@mui/material';
+import {Box, Collapse, List, ListItemButton, ListItemIcon, ListItemText, Table, TableBody, TableCell, TableRow} from '@mui/material';
 import {AppBarHeader} from '../../components/AppBar'
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import StarBorder from '@mui/icons-material/StarBorder';
+import iconHouse from '../../assets/icons/iconHouse.svg'
+import iconArrowDown from '../../assets/icons/iconArrowDown.svg'
 /**
 * @author
 * @function LayoutMain
@@ -9,15 +15,68 @@ import {AppBarHeader} from '../../components/AppBar'
 export const LayoutMain = (props) => {
   const [openSideBar, setOpenSideBar] = React.useState(false)
 
+  const [open, setOpen] = React.useState(true);
 
+  const handleClick = () => {
+    setOpen(!open);
+  };
   return (
     <Box display={'flex'} width={'100%'}>
       <Box minWidth={250} display={openSideBar ? 'block' : 'none'}>
-      tranlinhmobile
-        {/* <SideBar anchor={'left'} openSideBar={openSideBar}/> */}
+        <p style={{
+          color: 'black',
+          fontSize: '20px',
+          fontWeight: '600',
+        }}>tranlinhmobile</p>
+        {/* <Table>
+          <TableBody>
+            <TableCell>
+              <TableRow sx={{display: 'flex', alignItems: 'center', justifyContent:'space-between'}}>
+                <div style={{display:'flex'}}>
+                  <img src={iconHouse} alt='' />
+                  <div style={{
+                    color:'black',
+                    fontSize:'14px',
+                    fontWeight:'600',
+                    marginLeft:'15px'
+                  }}>Tổng quan</div>
+                </div>
+                <img src={iconArrowDown} alt='' />
+              </TableRow>
+            </TableCell>
+          </TableBody>
+        </Table> */}
+
+        <List>
+          <ListItemButton onClick={handleClick}>
+            <ListItemIcon sx={{
+              minWidth: '30px'
+            }}>
+              <img src={iconHouse} alt='' />
+            </ListItemIcon>
+            <ListItemText primary="Tổng quan" />
+            {open ? <ExpandLess /> : <ExpandMore />}
+          </ListItemButton>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              <ListItemButton sx={{pl: 4}}>
+                {/* <ListItemIcon>
+                  <StarBorder />
+                </ListItemIcon> */}
+                <ListItemText primary="Bán hàng" />
+              </ListItemButton>
+              <ListItemButton sx={{pl: 4}}>
+                {/* <ListItemIcon>
+                  <StarBorder />
+                </ListItemIcon> */}
+                <ListItemText primary="Dịch vụ - Bảo hành" />
+              </ListItemButton>
+            </List>
+          </Collapse>
+        </List>
       </Box>
       <Box width={'100%'}>
-        <AppBarHeader openSideBar={openSideBar} setOpenSideBar={setOpenSideBar}/>
+        <AppBarHeader openSideBar={openSideBar} setOpenSideBar={setOpenSideBar} />
         {props.children}
       </Box>
     </Box>
